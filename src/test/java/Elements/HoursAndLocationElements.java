f19470fb2979390cd2f3289a3c600bae5e05fd7e
jasonvalladolid <Jason>
2022-03-27 11:43:25 -0400

    1 st commit

--- src/test/java/Elements/HoursAndLocationElements.java
+++ src/test/java/Elements/HoursAndLocationElements.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 
 public class HoursAndLocationElements {
 
-	
+	//nw
 	
 	@FindBy(xpath ="//*[@id=\"footer-redesign-row-1-panels-container\"]/div[3]/label/ul/li[4]/a")
 	public WebElement HoursLocation;
